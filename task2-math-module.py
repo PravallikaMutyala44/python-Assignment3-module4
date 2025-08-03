563ae15d89ae0cb16f583ffcf2a1d4bb22295196
PravallikaMutyala44 <mutyalapravallika92@gmail.com>
2025-08-03 15:53:53 +0530

    Add files via upload

---
+++ task2-math-module.py
@@ -0,0 +1,10 @@
+import math
+num = float(input("Enter a number: "))
+
+sqrt_val = math.sqrt(num)
+log_val = math.log(num)
+sine_val = math.sin(num)
+
+print(f"Square root: {sqrt_val}")
+print(f"Logarithm: {log_val}")
+print(f"Sine: {sine_val}")
